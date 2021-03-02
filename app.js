7fe3ca9ca569bce1dfa5c0db3da59b24e0e1cab8
Brian Beeler <brianlbeeler@gmail.com>
2021-03-02 16:11:04 -0600

    Title change event handling added.

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     document.querySelector('#name').focus();
+    document.querySelector("#other-job-role").style.display = "none";
 });
+
+function onTitleChange (e) {
+    if (e.target.value === "other") {
+        document.querySelector("#other-job-role").style.display = "";
+    } else {
+        document.querySelector("#other-job-role").style.display = "none";
+    }
+} 
