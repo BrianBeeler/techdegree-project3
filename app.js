907e9ff5978e9f7bf8e70e9ed91a72bb0a07d554
Brian Beeler <brianlbeeler@gmail.com>
2021-03-05 15:14:32 -0600

    App totals payments.

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     document.querySelector('#name').focus();
-    document.querySelector("#other-job-role").style.display = "none";
-    enableColor(false);
+    renderOtherJobRole(false);
+    renderColor(false);
+    renderTotal(total);
 });
 
 function onTitleChange (e) {
     if (e.target.value === "other") {
-        document.querySelector("#other-job-role").style.display = "";
+        renderOtherJobRole();
     } else {
-        document.querySelector("#other-job-role").style.display = "none";
+        renderOtherJobRole(false);
     }
 } 
 
 function onDesignSelect (e) {
     if (e.target.value === "js puns") {
-        enableColor();
+        renderColor();
         showColors();
     } else if (e.target.value === "heart js") {
-        enableColor();
+        renderColor();
         showColors("heart js");
     } else {
-        enableColor(false);
+        renderColor(false);
     }
 
 }
 
-function enableColor(enable=true) {
+function renderOtherJobRole(enable = true) {
+    if (enable) {
+        document.querySelector("#other-job-role").style.display = "";
+    } else {
+        document.querySelector("#other-job-role").style.display = "none";
+    }
+  
+}
+
+function renderColor(enable=true) {
     if (enable) {
         document.querySelector("#shirt-colors").style.display = "";
     } else {
@@ -33,6 +43,11 @@ function enableColor(enable=true) {
     }
 
 }
+
+function renderTotal(total) {
+    document.querySelector('#activities-cost').innerHTML = `Total: $${total}`;
+}
+
 
 function showColors(colorsToShow = 'js puns') {
 
@@ -54,4 +69,16 @@ function showColors(colorsToShow = 'js puns') {
         shown[j].style.display = "";
     }
 
+}
+
+let total = 0;
+
+function add(e, amount) {
+ 
+    if (e.target.checked) {
+        total+=amount;
+    } else {
+        total-=amount;
+    }
+    renderTotal(total);
 }
