0fc6991c8d600a315f40bb1acf12303f9987dd6c
Brian Beeler <brianlbeeler@gmail.com>
2021-03-05 06:53:37 -0600

    Dynamic color select implimented.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     document.querySelector('#name').focus();
     document.querySelector("#other-job-role").style.display = "none";
+    enableColor(false);
 });
 
 function onTitleChange (e) {
@@ -13,11 +14,44 @@ function onTitleChange (e) {
 
 function onDesignSelect (e) {
     if (e.target.value === "js puns") {
-
+        enableColor();
+        showColors();
     } else if (e.target.value === "heart js") {
-    
+        enableColor();
+        showColors("heart js");
     } else {
-        
+        enableColor(false);
+    }
+
+}
+
+function enableColor(enable=true) {
+    if (enable) {
+        document.querySelector("#shirt-colors").style.display = "";
+    } else {
+        document.querySelector("#shirt-colors").style.display = "none";
+    }
+
+}
+
+function showColors(colorsToShow = 'js puns') {
+
+    let colorsToHide;
+
+    if (colorsToShow === 'js puns') {
+        colorsToHide = "heart js";
+    } else {
+        colorsToHide = 'js puns';
+    }
+
+    let hidden = document.querySelectorAll(`[data-theme='${colorsToHide}']`);
+    let shown = document.querySelectorAll(`[data-theme='${colorsToShow}'`);
+
+    for (let i = 0; i<hidden.length; i++) {
+        hidden[i].style.display = "none";
+    }
+    for (let j = 0; j< shown.length; j++) {
+        shown[j].style.display = "";
     }
 
 }
